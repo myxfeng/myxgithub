f851f47d6926f7077407c5ee667977cd57c678c0
MAYUXIN-PC\马玉鑫-pc <137560877@qq.com>
2016-10-11 21:16:56 +0800

    hhkhl

--- app/src/main/java/expamle/com/myapplication/MainActivity.java
+++ app/src/main/java/expamle/com/myapplication/MainActivity.java
@@ -11,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // 我是develop-myx
         ///////
-        ///222
-        ////
     }
 }
