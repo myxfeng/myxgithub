23f7f4c4644b2fcbb15f6ee8dfdede5fba383177
mayuxin <mayuxin@people.cn>
2017-12-23 14:29:30 +0800

    jjj

--- app/src/main/java/expamle/com/myapplication/MainActivity.java
+++ app/src/main/java/expamle/com/myapplication/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         //A测试1，，，
         //B 111111111
         //B 222222222
-        //B 3333333
+        //B 3333333jj
         //
         //
         ////jjj，，lll
